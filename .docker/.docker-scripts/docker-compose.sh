fe02debf465810f342773854754453ed0940d0a0
Young Joon Lee <entelecheia@hotmail.com>
2024-07-03 13:06:21 +0900

    refactor(docker-compose): use environment variables for project settings refactor(Dockerfile): switch to new base image and refactor user setup refactor(docker-compose.yaml): enhance environment variables and volume mappings refactor(docker): add entrypoint script for container initialization

--- .docker/.docker-scripts/docker-compose.sh
+++ .docker/.docker-scripts/docker-compose.sh
@@ -20,9 +20,9 @@ $0 build -v base
 "
 
 # declare arguments
-PROJECT_ID="default"
+PROJECT_ID=${DOCKER_PROJECT_ID:-"default"}
 COMMAND="build"
-VARIANT="base"
+VARIANT=${IMAGE_VARIANT:-"base"}
 RUN_COMMAND="bash"
 ADDITIONAL_ARGS=""
 
@@ -150,6 +150,7 @@ fi
 set +a
 
 # prepare docker network
+CONTAINER_NETWORK_NAME=${CONTAINER_NETWORK_NAME:-""}
 if [[ -n "${CONTAINER_NETWORK_NAME}" ]] && ! docker network ls | grep -q "${CONTAINER_NETWORK_NAME}"; then
     echo "Creating network ${CONTAINER_NETWORK_NAME}"
     docker network create "${CONTAINER_NETWORK_NAME}"
@@ -159,13 +160,20 @@ fi
 
 # prepare local workspace to be mounted
 echo "Preparing local workspace directories"
-[ ! -d "${HOST_WORKSPACE_ROOT}" ] && mkdir -p "${HOST_WORKSPACE_ROOT}"
-[ ! -d "${HOST_SCRIPTS_DIR}" ] && cp -r "$PWD/.docker/scripts" "${HOST_SCRIPTS_DIR}"
-[ ! -d "${HOST_SSH_DIR}" ] && mkdir -p "${HOST_SSH_DIR}"
-[ ! -d "${HOST_CACHE_DIR}" ] && mkdir -p "${HOST_CACHE_DIR}"
-[ ! -d "${HOST_HF_HOME}" ] && mkdir -p "${HOST_HF_HOME}"
-[ ! -d "${HOST_GH_CONFIG_DIR}" ] && mkdir -p "${HOST_GH_CONFIG_DIR}"
-[ ! -d "${HOST_PASSAGE_DIR}" ] && mkdir -p "${HOST_PASSAGE_DIR}"
+HOST_WORKSPACE_ROOT="${HOST_WORKSPACE_ROOT:-}"
+[ -n "${HOST_WORKSPACE_ROOT}" ] && [ ! -d "${HOST_WORKSPACE_ROOT}" ] && mkdir -p "${HOST_WORKSPACE_ROOT}"
+HOST_SCRIPTS_DIR="${HOST_SCRIPTS_DIR:-}"
+[ -n "${HOST_SCRIPTS_DIR}" ] && [ ! -d "${HOST_SCRIPTS_DIR}" ] && cp -r "$PWD/.docker/scripts" "${HOST_SCRIPTS_DIR}"
+HOST_SSH_DIR="${HOST_SSH_DIR:-}"
+[ -n "${HOST_SSH_DIR}" ] && [ ! -d "${HOST_SSH_DIR}" ] && mkdir -p "${HOST_SSH_DIR}"
+HOST_CACHE_DIR="${HOST_CACHE_DIR:-}"
+[ -n "${HOST_CACHE_DIR}" ] && [ ! -d "${HOST_CACHE_DIR}" ] && mkdir -p "${HOST_CACHE_DIR}"
+HOST_HF_HOME="${HOST_HF_HOME:-}"
+[ -n "${HOST_HF_HOME}" ] && [ ! -d "${HOST_HF_HOME}" ] && mkdir -p "${HOST_HF_HOME}"
+HOST_GH_CONFIG_DIR="${HOST_GH_CONFIG_DIR:-}"
+[ -n "${HOST_GH_CONFIG_DIR}" ] && [ ! -d "${HOST_GH_CONFIG_DIR}" ] && mkdir -p "${HOST_GH_CONFIG_DIR}"
+HOST_PASSAGE_DIR="${HOST_PASSAGE_DIR:-}"
+[ -n "${HOST_PASSAGE_DIR}" ] && [ ! -d "${HOST_PASSAGE_DIR}" ] && mkdir -p "${HOST_PASSAGE_DIR}"
 
 # run docker-compose
 if [ "${COMMAND}" == "push" ]; then
@@ -176,7 +184,7 @@ elif [ "${COMMAND}" == "login" ]; then
 elif [ "${COMMAND}" == "run" ]; then
     CMD="docker compose --project-directory . -f .docker/docker-compose.${VARIANT}.yaml run workspace ${RUN_COMMAND} ${ADDITIONAL_ARGS}"
 else
-    CMD="docker-compose --project-directory . -f .docker/docker-compose.${VARIANT}.yaml -p ${CONTAINER_HOSTNAME} ${COMMAND} ${ADDITIONAL_ARGS}"
+    CMD="docker-compose --project-directory . -f .docker/docker-compose.${VARIANT}.yaml -p ${CONTAINER_PROJECT_NAME} ${COMMAND} ${ADDITIONAL_ARGS}"
 fi
 echo "Running command: ${CMD}"
 eval "${CMD}"
